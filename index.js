c6c7a4bac019e28da56ef04e18089fedbbcaea43
Totowdu80 <totowdu80@gmail.com>
2020-03-25 15:52:41 +0100

    Adding gitignore and editing index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+const log = console.log
+
+log(chalk.blue.bgWhite('Angelique'));
+log(chalk.green('Fabrice'));
+log(chalk.red("Victor V"));
+log(chalk.yellow("Romain"));
